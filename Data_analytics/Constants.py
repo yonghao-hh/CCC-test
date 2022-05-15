596329360b3f8635a3df45c4a7da15140da08e22
yonghao <yonghaoh1@student.unimelb.edu.au>
2022-05-15 20:49:29 +0800

    update data analytics

--- Data_analytics/Constants.py
+++ Data_analytics/Constants.py
@@ -1,21 +1,15 @@
 # Constants
 GEOFILE = "lang_aurin.json"
 
-AURIN_INCOME = "income_aurin.json"
-
-AURIN_LANGUAGE = "lang_aurin.json"
-
-AURIN_HEALTH = "medi_aurin.json"
-
 LARGE_TWITTER = "twitter-melb.json"
 
 EMPLOYMENT = ["employment","job", "occupation", "income", "pay"]
 
-HEALTH = ["health", "hospital","sick","unwell","doctor", "medicine", "drug"]
+HEALTH = ["health", "hospital","sick","unwell","doctor", "medicine", "drug", "covid", "pandemic"]
 
 TOPICS = [['diversity'], EMPLOYMENT, HEALTH]
 
-READ_LIMIT = 100000
+READ_LIMIT = 10000
 
 END_OF_LINE_START = -2 #the Index of where a line should end if it is first line of the large twitter file
 
--- Data_analytics/download_corpus.py
+++ Data_analytics/download_corpus.py
@@ -1,3 +1,4 @@
 import nltk
 nltk.download('omw-1.4')
 nltk.download('wordnet')
+nltk.download('punkt')
