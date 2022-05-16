be9bdaca9c50370ff524b0f122dc8b86529bee9f
yonghao <yonghaoh1@student.unimelb.edu.au>
2022-05-16 22:14:43 +0800

    update preprocess_json.py

--- web_application/run.py
+++ web_application/run.py
@@ -1,68 +1,15 @@
-# from app import app
-# if __name__ == '__main__':
-#     app.run(debug = True)
-
-# couchdb -> http://localhost:5984/_utils/index.html 
-from flask import Flask, redirect, url_for, request, render_template, make_response, jsonify, abort
-# from couchdb import Server
-import couchdb
-from flaskext.couchdb import Document
+from flask import Flask, redirect, render_template, make_response, jsonify
 import json
 
 app = Flask(__name__, static_url_path='/static')
 app.debug=True
 
 
-# Opening JSON file
-# f = open('data/health.json')
-# data = json.load(f)
-# print(data)
-# for i in data['id']:
-#     print(i)
-# f.close()
-
-# import db
-username = "admin"
-password = "admin"
-#employment = couchdb.Database("http://172.26.130.73:5984/_utils/#database/employment/_all_docs")
-#employment.resource.credentials(username, password)
-# http://172.26.130.73:5984/_utils/#/_all_dbs
-#couch = couchdb.Server('http://172.26.130.73:5984/_utils/')
-#income = couch["employment"]
-username = "admin"
-password = "admin"
-diversity_search = couchdb.Database("http://172.26.130.73:5984/_utils/#database/diversity_search")
-diversity_search.resource.credentials = (username, password)
-diversity_search.view('_all_docs', include_docs=True)
-
-# data1=dict()
-# while(true):
-# for item in employment.view('_all_docs', group=True, group_level=1):
-#    print(item)
-#    data1[item.key] = item.value
-# print(data1)
-
-
-# class User(Document):
-#     doc_type = 'User'
-# @app.route('/db', methods=['GET', 'POST'])
-# def register():
-# 	user = {
-# 	"username":"media site", 
-# 	"email":"justmail@gmail.com",
-# 	"password":"encrypteddata"
-# 	}
-# 	db = server['muocouch'] #select the database
-# 	doc_id, doc_rev = db.save(user) #store your data in th database
-#       # Use the view function to fetch your data from CouchDB
-#    map_func = '''function(doc) { emit(doc.doc_rev, doc); }'''
-#    # Get all the data by running a query set
-#    myQuery = User.query(db, map_func, reduce_fun=None, reverse=True)
-#    q = [i['username'] for i in myQuery] # Loop out all the usernames from the database
-#    q2 = [i['email'] for i in myQuery] # Loop out all the email addresses from the database
-#    q3 = q+q2 # Merge both queries into a single list
-#    print(q3)
-# 	return "<h2>Your data should now be in the database</h2>"
+# username = "admin"
+# password = "admin"
+# diversity_search = couchdb.Database("http://172.26.130.73:5984/_utils/#database/diversity_search")
+# diversity_search.resource.credentials = (username, password)
+# diversity_search.view('_all_docs', include_docs=True)
 
 
 @app.errorhandler(400)
